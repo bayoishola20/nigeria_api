4ce997f4678565eee406be43df1d089667eb80a8
bayoishola20 <bayoishola20@yahoo.com>
2017-10-26 23:51:09 +0100

    add

--- public/main.js
+++ public/main.js
@@ -5,3 +5,21 @@ const states = [];
 const prom = fetch(api)
                 .then(blob => blob.json())
                 .then(data => states.push(...data))
+
+function findMatch(searchMatch, states) {
+    return states.filter(place => {
+        const regex = new RegExp(searchMatch, 'gi');
+        return place.state.match(regex);
+    });
+}
+
+function showMatch() {
+    const matchArray = findMatch(this.value, states);
+    console.log(matchArray);
+}
+
+const stateSearch = document.querySelector('.stateSearch');
+const searchDrop = document.querySelector('.searchDrop');
+
+stateSearch.addEventListener('change', showMatch);
+stateSearch.addEventListener('keyup', showMatch);
