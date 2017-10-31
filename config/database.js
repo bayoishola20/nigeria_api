12c80efc1f646239d17e39c1e1064791c077bb73
bayoishola20 <bayoishola20@yahoo.com>
2017-11-01 00:10:25 +0100

    add

--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database: process.env.NODE_ENV == 'production' ?'mongodb://bayoishola20:bayoishola20&@ds119675.mlab.com:19675/nigerian_api' : 'mongodb://localhost:27017/nigeria_api',
-    secret: 'Juggernaut',
-}
